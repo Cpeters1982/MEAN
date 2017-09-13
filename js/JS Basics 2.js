1f08fc61e6c22cbb70efc77b8576b68e9a1a4298
Chris Peters <Indiekills@gmail.com>
2017-09-12 21:31:59 -0700

    Changed some stuff after fresh install

---
+++ js/JS Basics 2.js
@@ -0,0 +1,20 @@
+// In this assignment, complete the function magic_multiply and ensure it passes all the test inputs and matches their results. Modify magic_multiply until all the tests pass. Do not create a new magic_multiply for each test!
+
+function magic_multiply(x, y) {
+
+    return x;
+}
+let test1 = magic_multiply(5 * 2);
+console.log(test1);
+
+let test2 = magic_multiply(0 * 0, 0 + 0, 0 - 0, 0 / 0);
+let message = ("All inputs " + test2);
+console.log(message, test2);
+// I know this is wrong... Am I close?
+
+let test3 = magic_multiply([1, 2, 3], 2);
+
+
+
+
+
