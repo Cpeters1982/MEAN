d610733e91a573b92399c9691cc1e018b780e80b
Chris Peters <Indiekills@gmail.com>
2017-10-14 21:34:35 -0700

    Nothing important

--- js/JS Basics 1.js
+++ js/JS Basics 1.js
@@ -1,41 +1,41 @@
 // Basics 1
 
-// let x = [];
-// console.log(x);
+let x = [];
+console.log(x);
 
-// let x = [];
-// x.push('coding', 'dojo', 'rocks');
-// console.log(x);
+let x = [];
+x.push('coding', 'dojo', 'rocks');
+console.log(x);
 
-// let x = [];
-// x.push('coding', 'dojo', 'rocks');
-// x.pop();
-// console.log(x);
+let x = [];
+x.push('coding', 'dojo', 'rocks');
+x.pop();
+console.log(x);
 
 
 // Basics 2
-// const y = [];
-// console.log(y);
+const y = [];
+console.log(y);
 
-// const y = [];
-// y.push(88);
+const y = [];
+y.push(88);
 // Nothing happened... Docs say that the value of a constant cannot be changed through re-assignment
 // or redecleration... but they also say we can push into an array in const... I have no clue.
 
 
 // Basic 3
-// let z = [9,10,6,5,-1,20,13,2];
-// for(let i = 0; i < z.length-1; i++){
-//     console.log(z[i]);
-// }
+let z = [9,10,6,5,-1,20,13,2];
+for(let i = 0; i < z.length-1; i++){
+    console.log(z[i]);
+}
 
 
 // Basic 4
-// let names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
-// let lengths = names.map(function(word) {
-//     return word + ' = ' + word.length;
-// });
-// console.log(lengths);
+let names = ['Kadie', 'Joe', 'Fritz', 'Pierre', 'Alphonso'];
+let lengths = names.map(function(word) {
+    return word + ' = ' + word.length;
+});
+console.log(lengths);
 
 
 function yell(string){
